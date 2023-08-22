6ee3ec8bb0f8c9dcb6d7a49a37bb4f890150c49a
Miguel Jimenez <57577766+Zentrax997@users.noreply.github.com>
2023-08-22 10:43:26 -0500

    Add files via upload

---
+++ FormReg/script.js
@@ -0,0 +1,34 @@
+const $btnSignIn= document.querySelector('.sign-in-btn'),
+      $btnSignUp = document.querySelector('.sign-up-btn'),  
+      $signUp = document.querySelector('.sign-up'),
+      $signIn  = document.querySelector('.sign-in');
+
+document.addEventListener('click', e => {
+    if (e.target === $btnSignIn || e.target === $btnSignUp) {
+        $signIn.classList.toggle('active');
+        $signUp.classList.toggle('active')
+    }
+});
+
+// ACA ES DONDE VA EL QUEARY DEL MENU DESPEGABLE
+    const Iconomenu = document.querySelector('#Icono-menu'),
+            menu = document.querySelector('#menu'); 
+
+    Iconomenu.addEventListener('click', (e)=>{
+
+        menu.classList.toggle('active');
+        document.body.classList.toggle('opacity');
+
+        const rutaActual = e.target.getAttribute('src');
+
+        if(rutaActual == 'rayita.png'){
+            e.target.setAttribute('src','rayitablanca.png');
+        }else{
+            e.target.setAttribute('src','rayita.png');
+        }
+
+    });
+
+
+
+
